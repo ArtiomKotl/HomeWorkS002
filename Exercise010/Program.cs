a04c2bec7ff6f08021b23bbcec43c41d826f5270
artketler <artketler.ak@gmail.com>
2023-01-21 22:21:40 +0300

    Exercise 010_ok project

--- Exercise010/Program.cs
+++ Exercise010/Program.cs
@@ -6,3 +6,15 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число и я выведу вам вторую цифру этого числа");
+Console.Write(": ");
+if (!int.TryParse(Console.ReadLine(), out int number) || number < 100 || number > 999)
+{
+  Console.Write("Неправильный ввод данных! Введите трёхзначное число: ");
+  return;
+}
+int newnumber = number / 10;
+int secondDigit = newnumber % 10;
+Console.WriteLine("Вторая цифра числа " + number + " это " + secondDigit); 
+Console.ReadKey();
